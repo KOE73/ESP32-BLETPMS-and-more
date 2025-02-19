204c1403041dc5ca1f08fdfa6e1a9b66b53dfd7e
KOEsoft <koetmp@gmail.com>
2025-02-19 22:05:05 +0500

    gz

--- extra_script.py
+++ extra_script.py
@@ -8,7 +8,9 @@ files_to_compress = ["src/www/index.html", "src/www/css.css"]
 def compress_files():
     for file in files_to_compress:
         input_path = Path(file)
-        output_path = input_path.with_suffix(input_path.suffix + ".gz")
+        output_path = input_path.parent / ".gz" / input_path.name
+        output_path = output_path.with_suffix(output_path.suffix + ".gz")
+        # output_path = input_path.with_suffix(input_path.suffix + ".gz")
 
         # Открываем файл и создаем GZIP-архив
         with open(input_path, "rb") as f_in, gzip.open(output_path, "wb") as f_out:
